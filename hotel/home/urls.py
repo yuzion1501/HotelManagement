c0e7007eea4b535ffe64874c5e12c88cd67fdf45
Khuong Luan <117499918+makhuong02@users.noreply.github.com>
2023-06-18 20:31:21 +0700

    Delete hotel directory

--- hotel/home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('home/', views.turn_to_home, name='home'),
-    path('reservation/', views.reservation, name='reservation'),
-]
